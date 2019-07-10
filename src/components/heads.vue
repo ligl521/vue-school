230ff40fc6d680c03c46dbd09dec7a0b27b58b8f
wangxinyu <wang.xinyu7@outlook.com>
2019-07-10 17:59:07 +0800

    z

--- src/components/heads.vue
+++ src/components/heads.vue
@@ -15,31 +15,39 @@
       <el-menu-item index="4">教育人才库</el-menu-item>
       <el-menu-item index="5">项目数据库</el-menu-item>
       <el-menu-item index="6">数据可视化</el-menu-item>
-      <el-button round class="login_btn" type="text" @click="dialogVisible = true ;get_msg()">登录</el-button>
+      <el-button round class="login_btn" type="text" @click="dialogVisible = true;get_msg()">登录</el-button>
       <!-- 点击触发登录dialog -->
     </el-menu>
     <el-dialog
       :visible.sync="dialogVisible"
       width="17%"
       center
-      modal-append-to-body="false"
-      append-to-body="true"
+      :modal-append-to-body="true"
+      :append-to-body="true"
     >
       <el-tabs stretch v-model="activeName" @tab-click="handleClick">
         <!-- 二维码登录 -->
         <el-tab-pane label="扫码登录" name="first">
           <span>
             <img id="qr_code" src />
+            <p class="span_text"></p>
           </span>
         </el-tab-pane>
         <!-- 账号密码登录 -->
         <el-tab-pane label="账号密码" name="second">
-          <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="demo-ruleForm" show-message="false" status-icon="true">
+          <el-form
+            :model="ruleForm"
+            :rules="rules"
+            ref="ruleForm"
+            class="demo-ruleForm"
+            :show-message="true"
+            :status-icon="true"
+          >
             <el-form-item prop="userMail">
-            <el-input v-model="ruleForm.userMail" prop="userMail" placeholder="用户名"></el-input>
+              <el-input v-model="ruleForm.userMail" prop="userMail" placeholder="用户名"></el-input>
             </el-form-item>
             <el-form-item prop="pwd">
-            <el-input type="password" v-model="ruleForm.pwd" prop="pwd" placeholder="密码"></el-input>
+              <el-input type="password" v-model="ruleForm.pwd" prop="pwd" placeholder="密码"></el-input>
             </el-form-item>
             <el-form-item>
               <el-button type="primary" @click="send_msg('ruleForm')">登录</el-button>
@@ -60,7 +68,8 @@
 </template>
 <script>
 import axios from "axios";
-import { constants } from 'fs';
+import { constants } from "fs";
+import { setTimeout, setInterval } from "timers";
 export default {
   data() {
     return {
@@ -92,28 +101,31 @@ export default {
     },
     //账号密码传给后台
     send_msg(formName) {
-      this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          console.log("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+      let self = this;
+      var params = new URLSearchParams();
+      params.append("userName", self.ruleForm.userMail);
+      params.append("passWord", self.ruleForm.pwd);
+      if (params.userName != "" && params.passWord != "") {
+        axios({
+          method: "post",
+          url: "http://192.168.0.28:8080/nsi-1.0/user/login.do",
+          data: params
+        }).then(function(respons) {
+          if (respons.data.code == 0) {
+            alert(respons.data.msg);
           } else {
-            console.log('error submit!!');
-            return false;
+            alert(respons.data.msg);
           }
         });
-      let self = this;
-      var params = {
-         userName:self.ruleForm.userMail,
-         passWord:self.ruleForm.userMail
       }
-      console.log(self.ruleForm.userMail)
-      console.log(self.ruleForm.pwd)
-      axios({
-        method: "post",
-        url: "http://192.168.0.28:8080/nsi-1.0/user/login.do",
-        data: params
-      }).then(function(respons) {
-        console.log(respons);
-      });
     },
     //获取后台给的二维码
     get_msg() {
@@ -121,8 +133,57 @@ export default {
         method: "post",
         url: "http://data.xinxueshuo.cn/nsi-1.0/user/get_qrcode.do"
       }).then(function(response) {
-        console.log(response);
         document.getElementById("qr_code").src = response.data.data.qrCode;
+        var sceneStr = response.data.data.scenStr;
+        console.log(sceneStr);
+        //微信扫码登录轮询
+        //倒计时
+        var totalTime = 120;
+        let clock = window.setInterval(() => {
+          totalTime--;
+          console.log(totalTime);
+          if (totalTime <= 0) {
+            window.clearInterval(clock);
+            console.log($(".span_text").html("二维码已失效，请从新打开二维码"));
+            window.clearInterval(t1);
+            totalTime = 120;
+          }
+        }, 1000);
+        var t1 = window.setInterval(() => {
+          axios
+            .get(
+              "http://data.xinxueshuo.cn/nsi-1.0/user/get_check_login.do?sceneStr=" +
+                sceneStr
+            )
+            .then(function(response) {
+              console.log(response.data);
+              if (response.data.code == 0) {
+                window.clearInterval(t1);
+                window.clearInterval(clock);
+                console.log(response.data.data.headimgurl); //用户头像
+                var OpenId = response.data.data.openid;//用户微信ID
+                axios
+                  .get(
+                    "http://data.xinxueshuo.cn/nsi-1.0/user/WechatLogin.do?OpenId=" +
+                      OpenId
+                  )
+                  .then(function(response) {
+                    console.log(response.data);
+                    if (response.data.code == 0) {
+                      document.cookie="username="+response.data.data.username;//用户邮箱
+                      document.cookie="memberSign="+response.data.data.memberSign;//用户等级
+                      document.cookie="UserVerifyCode="+response.data.data.userRegistercode;//用户检验码
+                    }
+                  })
+                  .catch(function(error) {
+                    console.log(error);
+                  });
+              }
+            })
+            .catch(function(error) {
+              console.log(error);
+            });
+        }, 3000);
       });
     },
     handleClick(tab, event) {
