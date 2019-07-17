ec71a0ebc9b8938fe29c82139e6bd984e0daeaf1
hhhslh <3064063267@qq.com>
2019-07-17 15:13:43 +0800

    s0717

--- src/api/api.js
+++ src/api/api.js
@@ -12,11 +12,21 @@ export const getSchoolHomeInquiry = p => get('/article/list.do', p)
 export const getSchoolCity = p => get('/new/school/list.do', p)
     //微信绑定
 export const wechatBind = p => get('/user/WechatBinding.do', p)
-    //注册
-export const register = p => post('/user/register.do', p)
+    //手机号注册
+export const register = p => post('/user/phoneRegister.do', p)
     //验证是否注册过
 export const isRegister = p => post('/user/UserMailCheck.do', p)
     //获取验证码
 export const getVerifyCode = p => post('/CommonApi/sendSms.do', p)
     //验证短信验证码
 export const checkVerifyCode = p => post('/CommonApi/check_code.do', p)
+    //手机号登录
+export const login = p => post('/user/login.do', p)
+    //获取二维码
+export const getQrcode = p => post('/user/get_qrcode.do', p)
+    //微信扫码登录轮询
+export const getCheckLogin = p => get('/user/get_check_login.do', p)
+    //微信Id登录
+export const weChatLogin = p => get('/user/WechatLogin.do', p)
+    //验证是否上传头像
+export const CheckImg = p => post('/manager/talent/upload.do', p)
